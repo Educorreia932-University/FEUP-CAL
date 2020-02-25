5505fc5f2413752586c9432d73988d6e9741d359
Educorreia932 <skelozard1@gmail.com>
2020-02-25 21:49:05 +0000

    Did Sum exercise

--- cal_fp01_CLion/Tests/Sum.cpp
+++ cal_fp01_CLion/Tests/Sum.cpp
@@ -5,8 +5,49 @@
 #include "Sum.h"
 #include <chrono>
 
-string calcSum(int* sequence, int size)
-{
-	return "";
+// Exercise 3. a)
+string calcSum(int* sequence, int size) {
+    int sum, min_sum;
+    string pair, result;
+
+    // Number of contiguous digits
+    for (int i = 0; i < size; i++) {
+        min_sum = INT_MAX;
+
+        // Groupings
+        for (int j = 0; j < size - i; j++) {
+            sum = 0;
+
+            for (int k = j; k < j + i + 1; k++)
+                sum += sequence[k];
+
+            if (sum < min_sum) {
+                pair = to_string(sum) + "," + to_string(j) + ";";
+                min_sum = sum;
+            }
+        }
+
+        result += pair;
+    }
+
+    return result;
 }
 
+// Exercise 3. b)
+int main() {
+    for (int i = 0; i < 500; i++) {
+        int* sequence = new int[i];
+
+        srand(time(nullptr));
+
+        for (int j = 0; j < i; j++)
+            sequence[i] = rand();
+
+        auto start = chrono::high_resolution_clock::now();
+
+        calcSum(sequence, i);
+
+        auto finish = chrono::high_resolution_clock::now();
+        auto mili = chrono::duration_cast<chrono::milliseconds>(finish - start).count();
+    }
+}
--- cal_fp01_CLion/Tests/tests.cpp
+++ cal_fp01_CLion/Tests/tests.cpp
@@ -36,13 +36,13 @@ TEST(CAL_FP01, CalcChangeTest) {
 	EXPECT_EQ("4;4;",calcChange(8, numCoins3, coinValues3));
 }
 
-/*TEST(CAL_FP01, CalcSumArrayTest) {
+TEST(CAL_FP01, CalcSumArrayTest) {
 	int sequence[5] = {4,7,2,8,1};
 	int sequence2[9] = {6,1,10,3,2,6,7,2,4};
 
 	EXPECT_EQ("1,4;9,1;11,2;18,1;22,0;",calcSum(sequence, 5));
 	EXPECT_EQ("1,1;5,3;11,3;16,1;20,3;24,3;31,1;35,1;41,0;",calcSum(sequence2, 9));
-}*/
+}
 
 /*TEST(CAL_FP01, PartitioningTest) {
 	EXPECT_EQ(3025,s_recursive(9,3));
