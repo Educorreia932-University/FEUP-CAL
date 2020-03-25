555c8eba935295a7e20b0807f88eef17de478248
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2020-03-25 22:15:38 +0000

    save to csv clean data

--- MapExtraction/Example/getGraph.py
+++
@@ -1,7 +0,0 @@
-import osmnx as ox 
-
-ox.config(log_console=True, use_cache=True) 
-graph = ox.graph_from_place("Manhattan, New York, New York, USA" ,network_type = 'drive')
-fig, ax = ox.plot_graph(graph)
-
-ox.save_load.save_graph_shapefile(graph, "Los_Angeles")
