ffe0eea7d306667f37679262fd83faba9197ea9e
Educorreia932 <skelozard1@gmail.com>
2020-04-06 22:36:10 +0100

    Removed area field from edges.csv

--- MapExtraction/getGraph.py
+++ MapExtraction/getGraph.py
@@ -2,9 +2,8 @@ import osmnx as ox
 import networkx as nx
 import pandas as pd
 
-
 def cleanData(df):
-    df.drop(["geometry", "ref", "tunnel", "bridge", "lanes", "highway", "width", "junction", "access", "service"],
+    df.drop(["geometry", "ref", "tunnel", "bridge", "lanes", "highway", "width", "junction", "access", "service", "area"],
             axis=1, inplace=True)
     df['maxspeed'] = df['maxspeed'].fillna(0)
     df['name'] = df['name'].fillna("NONE")
@@ -12,14 +11,11 @@ def cleanData(df):
     print(df.columns)
     return df
 
-
 def getNodes(graph):
     return pd.DataFrame(list(graph.nodes))
 
-
 def getEdges(graph):
     return nx.to_pandas_edgelist(graph)
-
 
 # It saves the data in csv by node and edges
 def saveData(graph):
@@ -28,13 +24,11 @@ def saveData(graph):
     edges.to_csv('edges.csv', index=False, encoding="latin1")
     getNodes(graph).to_csv('nodes.csv', index=False)
 
-
 def main(cityName, placeName):
     ox.config(log_console=True, use_cache=True)
     graph = ox.core.graph_from_place(placeName, network_type='drive')
     ox.plot_graph(graph, save=True, filename=cityName, file_format="png")
     ox.extended_stats(graph, ecc=True)
     saveData(graph)
-
 
 main("Porto", "Município do Porto, Portugal")
