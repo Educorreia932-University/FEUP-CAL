b637fbc7fddbab6ee58aaa869bb4a6b744158d22
a3brx <a3brx@sapo.pt>
2020-05-06 13:02:13 +0100

    correct path

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,6 @@ int main() {
     UserInterface ui;
     //ui.mainMenuSelection(ui.showMainMenu());
     auto *g = new GraphFactory();
-    g->readVertex("../../MapExtraction/nodes.csv");
+    g->readVertex("../../utils/map_extraction/nodes.csv");
     return 0;
 }
