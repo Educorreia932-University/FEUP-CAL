24ba32daf5287e83f4277b6299ace460b395d1c8
Jumaruba <juliane.marubayashi@gmail.com>
2020-05-09 18:08:39 +0100

    test failling

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,25 @@
 
 using namespace std;
 
+void printTest(vector<ulli> res);
+void test1_floyd();
+void test2_floyd();
+
 int main() {
     UserInterface ui;
     //ui.mainMenuSelection(ui.showMainMenu());
     auto *g = new GraphFactory();
     g->readVertex("../../utils/map_extraction/nodes.csv");
+
+    test2_floyd();
+
+    return 0;
+}
+
+
+//floyd warshall test1
+void test1_floyd(){
+
     auto graph = new Graph();
     vector<Vertex *> vertexes;
     for (int i = 0; i < 10; ++i) {
@@ -21,7 +35,35 @@ int main() {
     graph->floydWarshallShortestPath();
     auto res = graph->getFloydWarshallPath(9, 0);
 
+    printTest(res);
+}
+
+//test floydWarshall 2
+void test2_floyd(){
+    vector<ulli> v = {1,2,3,4,5,6};
+    auto graph = new Graph();
+
+    for (int i = 1 ; i < 7; i++ ) graph->addVertex(i);
+    graph->addEdge(1,2,1);
+    graph->addEdge(1,3,3);
+    graph->addEdge(2,3,1);
+    graph->addEdge(2,4,5);
+    graph->addEdge(2,5,2);
+    graph->addEdge(3,5,4);
+    graph->addEdge(4,6,1);
+    graph->addEdge(4,5,1);
+    graph->addEdge(5,4,1);
+    graph->addEdge(5,6,4);
+
+    graph->floydWarshallShortestPath();
+    auto res = graph->getFloydWarshallPath(1,6);
+
+    printTest(res);
+
+}
+
+void printTest(vector<ulli> res){
     for (auto elem : res)
         cout << elem << ' ';
-    return 0;
+    cout << endl;
 }
