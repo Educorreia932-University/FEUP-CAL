9325356690af6a448790a134cda5c94551c9ff52
Jumaruba <juliane.marubayashi@gmail.com>
2020-05-14 17:08:21 +0100

    building the floydStorage

---
+++ src/Graph/floydStorage.cpp
@@ -0,0 +1,5 @@
+//
+// Created by julia on 14/05/2020.
+//
+
+#include "floydStorage.h"
---
+++ src/Graph/floydStorage.h
@@ -0,0 +1,16 @@
+//
+// Created by julia on 14/05/2020.
+//
+
+#ifndef SRC_FLOYDSTORAGE_H
+#define SRC_FLOYDSTORAGE_H
+
+
+
+class floydStorage {
+
+};
+
+
+
+#endif //SRC_FLOYDSTORAGE_H
